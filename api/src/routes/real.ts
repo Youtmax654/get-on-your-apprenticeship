9b12e52d8e1f1c215ee229c342cdc66661866c5a
Youtmax <youtmax654@gmail.com>
2024-04-10 22:48:34 +0200

    Firestore hosting students data

--- api/src/routes/real.ts
+++ api/src/routes/real.ts
@@ -1,29 +1,35 @@
+import { Firestore } from "@google-cloud/firestore";
 import express from "express";
 const realRouter = express.Router();
+
+const db = new Firestore({
+  projectId: "teolia-apprentice",
+  keyFilename: "./src/keyfile.json",
+});
+
+const studentsCollection = db.collection("students");
+
+const fetchStudents = async () => {
+  const students = await studentsCollection.get();
+  console.log(students.docs.map((student) => student.data()));
+  return students.docs.map((student) => student.data());
+};
 
 realRouter.get("/", function (req, res, next) {
   res.send("This is real route");
 });
 
 realRouter.get("/students", function (req, res, next) {
-  fetch(
-    "https://harry-potter-api-3a23c827ee69.herokuapp.com/api/characters/students"
-  )
-    .then((res) => res.json())
-    .then((data: any) => {
-      res.json(data);
-    });
+  fetchStudents().then((data: any) => {
+    res.json(data);
+  });
 });
 
 realRouter.get("/randomstudent", function (req, res, next) {
-  fetch(
-    "https://harry-potter-api-3a23c827ee69.herokuapp.com/api/characters/students"
-  )
-    .then((res) => res.json())
-    .then((data: any) => {
-      const randomIndex = Math.floor(Math.random() * data.length);
-      res.json(data[randomIndex]);
-    });
+  fetchStudents().then((data: any) => {
+    const randomIndex = Math.floor(Math.random() * data.length);
+    res.json(data[randomIndex]);
+  });
 });
 
 export default realRouter;
