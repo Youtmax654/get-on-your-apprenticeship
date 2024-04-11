9e5c073dc0d411f262013b1b8a0777b03062c427
Youtmax <youtmax654@gmail.com>
2024-04-11 12:35:52 +0200

    Modification for Netlify deployment (API)

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,32 +4,32 @@
  * Module dependencies.
  */
 
-import app from './app';
-import debug from 'debug';
-debug('api:server');
+import debug from "debug";
+import app from "./app";
+debug("api:server");
 
-import http from 'http';
+import serverless from "serverless-http";
 
 /**
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
 
 /**
  * Create HTTP server.
  */
 
-const server = http.createServer(app);
+export const server = serverless(app);
 
 /**
  * Listen on provided port, on all network interfaces.
  */
 
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+// server.listen(port);
+// server.on("error", onError);
+// server.on("listening", onListening);
 
 /**
  * Normalize a port into a number, string, or false.
@@ -56,20 +56,20 @@ function normalizePort(val: string) {
  */
 
 function onError(error: Error & { syscall: string; code: string }) {
-  if (error.syscall !== 'listen') {
+  if (error.syscall !== "listen") {
     throw error;
   }
 
-  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
       process.exit(1);
       break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
       process.exit(1);
       break;
     default:
@@ -81,8 +81,8 @@ function onError(error: Error & { syscall: string; code: string }) {
  * Event listener for HTTP server "listening" event.
  */
 
-function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr?.port;
-  debug('Listening on ' + bind);
-}
+// function onListening() {
+//   const addr = server.address();
+//   const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr?.port;
+//   debug("Listening on " + bind);
+// }
